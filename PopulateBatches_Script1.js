12783c9c7ecfa98056bab2621736dd539d768908
rmanohar <reshumanohar@yahoo.com>
2025-05-26 13:35:41 -0400

    Create PopulateBatches_Script1.js

---
+++ PopulateBatches_Script1.js
@@ -0,0 +1,58 @@
+<script runat="server" type="text/javascript">
+  Platform.Load("Core",'1.1');
+  
+  var rowsData;
+  var recordCount = 0;
+  var source = DataExtension.Init("ContactsToBeDeleted");
+  var totalProcessed = Platform.Function.Lookup('CA_CustomObjectKeyRange','StartRowIndex','Key','ContactsToBeDeleted_2025');
+  var depre = "CA_2025_Batch";
+  var bkpre = "CA_2025_BKUP_Batch";
+  
+  try {	
+      for(s = 1; s<3; s++){
+      var dekey = depre.concat(s);
+      var DE = DataExtension.Init(dekey);
+      var bkkey = bkpre.concat(s);
+      var BK = DataExtension.Init(bkkey);
+      var batchprocessed = 0;
+      var arr = [];
+      var max = 0;
+      var log1 = Platform.Function.InsertData("CA_DebugLogs",["log"],["Batch DE Key = " + dekey]);
+    
+      do {    
+        rowsData = source.Rows.Retrieve({Property:"RowKey",SimpleOperator:"greaterThanOrEqual",Value:totalProcessed});
+        recordCount = rowsData.length;
+        
+        for( i = 0; i < recordCount; i++) {
+          var SubKey = rowsData[i].SubscriberKey;
+          var EmailAddr = rowsData[i].EmailAddress;
+          max = rowsData[i].RowKey;
+          var payload = {
+            SubscriberKey: SubKey,
+            EmailAddress: EmailAddr
+            };
+          var addedRowCount = DE.Rows.Add(payload);			
+          var addedRowCount2 = BK.Rows.Add(payload);					
+          arr.push(max);
+          }
+        arr.sort(function(a, b){return b-a});
+        totalProcessed = arr[0];
+        batchprocessed += recordCount;	
+            
+        if(parseFloat(batchprocessed) >= 50000) {			
+          break;
+        }
+        totalProcessed ++;   
+      } while (recordCount > 0) 
+    
+    var processlog = Platform.Function.InsertData("CA_ProcessLog",["BatchName","LastRowKey", "BatchNum"],["S-"+s,totalProcessed, s]);
+    var dbuglog = Platform.Function.InsertData("CA_DebugLogs",["log"],["Last processed row after the while Loop in " + "s" + s+ " = " + totalProcessed]);
+    totalProcessed ++;
+    }
+  
+  } catch(e) {
+      var debugDE = DataExtension.Init("CA_DebugLogs");
+        var arrDebug = [{log: 'Error in CA_2025_PopulateBatches js: ' + Stringify(e)}];
+        debugDE.Rows.Add(arrDebug);
+}      
+</script>
